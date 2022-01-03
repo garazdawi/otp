d653e9359073820a7e8a389148592d763d950821
Lukas Larsson <lukas@erlang.org>
2022-01-03 09:43:42 +0100

    asd

--- .github/scripts/pr-comment.js
+++ .github/scripts/pr-comment.js
@@ -8,34 +8,57 @@ module.exports = async({ github, context, body }) => {
         return `https://nightly.link/${context.repo.owner}/${context.repo.repo}/actions/artifacts/${artifact.id}.zip`
     };
 
-    const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        run_id: context.payload.workflow_run.id,
-        per_page: 100
-    });
+    const artifacts = github.paginate(
+        github.rest.actions.listWorkflowRunArtifacts,
+        {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            run_id: context.payload.workflow_run.id
+        }).then(a => a);
 
-    const ct_logs = artifacts.data.artifacts.find(
+    console.log(JSON.stringify(artifacts,null,2));
+
+    const ct_logs = artifacts.find(
         (a) => { return a.name == 'test_results'; });
-    const html_docs = artifacts.data.artifacts.find(
+    const html_docs = artifacts.find(
         (a) => { return a.name == 'otp_doc_html'; });
-    const win_exe = artifacts.data.artifacts.find(
+    const win_exe = artifacts.find(
         (a) => { return a.name == 'otp_win32_installer'; });
+
+    const gh_comments = github.paginate(
+        github.rest.issues.listComments,
+        {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: context.payload.workflow_run.pull_requests[0].number
+        }).then((comments, done) => {
+            for (const c of comments) {
+                console.log(JSON.stringify(c,null,2));
+                if (c.user.login.match(/github-actions[bot]/)) {
+                    done();
+                    return [c];
+                }
+            }
+            return [];
+        });
+
+    console.log(JSON.stringify(gh_comment,null,2));
+    let oldBody = gh_comments[0].body;
 
     // If the body already contains an Artifacts header, this is a call to
     // update the comment without having updated any test results. So
     // We need to remove the intro and extro from the body
-    if (/##/.test(body)) {
-        body = body.split("##")[1];
+    if (/##/.test(oldBody)) {
+        oldBody = oldBody.split("##")[1];
     }
 
-    return `Hello!
+    const newBody = `Hello!
 
 Thanks for opening a PR to improve Erlang/OTP.
 
 To speed up review, make sure that you have read [Contributing to Erlang/OTP](https://github.com/erlang/otp/blob/master/CONTRIBUTING.md) and that all [checks](/${context.repo.owner}/${context.repo.repo}/pull/${ context.issue.number }/checks) pass.
 
-${body}
+${oldBody}
 
 ## Artifacts
 * ` + (ct_logs ? `[Complete CT logs](${nightlyURL(ct_logs)})` : "No CT logs found") + `
@@ -46,4 +69,10 @@ See the [TESTING](https://github.com/erlang/otp/blob/master/HOWTO/TESTING.md) an
 
 // Erlang/OTP Github Action Bot
 `;
+    await github.rest.issues.updateComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: gh_comments[0].id,
+        body: newBody
+    });
 };
