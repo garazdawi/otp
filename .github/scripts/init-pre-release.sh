3887faf66bc91d3e037b97fe8a962d53a725b792
Lukas Larsson <lukas@erlang.org>
2022-10-05 21:43:29 +0200

    Try to create cache

--- .github/scripts/init-pre-release.sh
+++ .github/scripts/init-pre-release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ## We create a tar ball that is used later by build-otp-tar
 ## to create the pre-built tar ball
@@ -23,6 +23,22 @@ if [ ! -f configure ]; then
     git commit --no-verify -m 'Add generated configure files'
     AUTOCONF=1
 fi
+
+## if possible, we use tar to create the archive as that preserves the timestamps
+## of the files. If we use git archive then the timestamps of all files are from
+## the latest commit, not of the latest commit for each file
+# UNTRACKED=$(git status -s -unormal | grep "^??" | awk '{print $2}')
+# EXCLUDE_UNTRACKED=("--exclude=.git")
+# for file in $UNTRACKED; do
+#     if [ -d $file ]; then
+#         EXCLUDE_UNTRACKED=("--exclude=$file*" "${EXCLUDE_UNTRACKED[@]}")
+#     else
+#         EXCLUDE_UNTRACKED=("--exclude=$file" "${EXCLUDE_UNTRACKED[@]}")
+#     fi
+# done
+# if ! tar -czf otp_src.tar.gz ${EXCLUDE_UNTRACKED[@]} --transform "s/^./otp/" .; then
+#     git archive --prefix otp/ -o otp_src.tar.gz HEAD
+# fi
 git archive --prefix otp/ -o otp_src.tar.gz HEAD
 
 if [ "$AUTOCONF" = 1 ]; then
---
+++ .github/scripts/restore-from-prebuilt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -xe
+
+CACHE=$1
+shift
+ARCHIVE=$1
+shift
+TARGET=$1
+shift
+DELETED="$*"
+
+TMP_DIR=$(mktemp -d)
+CACHE_DIR="${TMP_DIR}"
+ARCHIVE_DIR="${TMP_DIR}/archive"
+
+mkdir "${ARCHIVE_DIR}"
+tar -C "${CACHE_DIR}/" -xzmf "${CACHE}"
+mkdir "${CACHE_DIR}/cache"
+cp -rp "${CACHE_DIR}/otp" "${CACHE_DIR}/cache/"
+sleep 5
+tar -C "${ARCHIVE_DIR}/" -xzmf "${ARCHIVE}"
+
+rsync -avcu --no-t "${ARCHIVE_DIR}/otp/" "${CACHE_DIR}/otp/"
+
+for delete in $DELETED; do
+    if [ -d "${CACHE_DIR}/otp/${delete}" ]; then
+        rm -r "${CACHE_DIR}/otp/${delete}"
+    elif [ -f "${CACHE_DIR}/otp/${delete}" ]; then
+        rm "${CACHE_DIR}/otp/${delete}"
+    else
+        echo "Could not find $delete to delete"
+        exit 1
+    fi
+done
+
+# tar -czf "${TARGET}" -C "${TMP_DIR}" otp
+
+# rm -rf "${TMP_DIR}"
