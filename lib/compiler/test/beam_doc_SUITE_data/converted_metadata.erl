7ac73bcee27bc9fc9e49f6e79fd8a3b598f45d46
Lukas Backström <lukas@erlang.org>
2024-12-13 16:22:31 +0100

    Stash

---
+++ lib/compiler/test/beam_doc_SUITE_data/converted_metadata.erl
@@ -0,0 +1,8 @@
+-module(converted_metadata).
+
+-moduledoc(#{ authors => ["me","myself","I"], format => "custom", since => "1.0", deprecated => "yes" }).
+
+-export([test/0]).
+
+-doc #{ since => "1.0", deprecated => "yes", equiv => "other", group => "collection" }.
+test() -> ok.
