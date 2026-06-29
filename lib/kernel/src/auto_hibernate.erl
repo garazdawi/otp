9c4346f189eb01b625877a6f032baefe57146c29
Lukas Backström <lukas@erlang.org>
2026-06-29 18:29:40 +0200

    kernel: add auto_hibernate service
    Implements step 2 of idea #1: an opt-in gen_server (auto_hibernate) that
    periodically scans the node's processes and hibernates the ones that have been
    idle for a configurable number of scan rounds. Idleness is detected from
    reduction counts (a process whose reductions are unchanged between two scans
    did no work), so no per-process instrumentation is required.
    
    With {compressed, true} the idle processes' heaps are additionally compressed
    (erlang:hibernate/2). Processes are not hibernated again until they do some
    work, and an exclusion list/predicate is supported. The service is not started
    by the kernel boot; start it with auto_hibernate:start_link/0,1.

---
+++ lib/kernel/src/auto_hibernate.erl
@@ -0,0 +1,232 @@
+%%
+%% %CopyrightBegin%
+%%
+%% SPDX-License-Identifier: Apache-2.0
+%%
+%% Copyright Ericsson AB 2026. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% %CopyrightEnd%
+%%
+-module(auto_hibernate).
+-moduledoc """
+Automatic hibernation of idle processes (idea #1, step 2).
+
+This is a `m:gen_server` that periodically scans the processes of the node and
+hibernates the ones that have been idle for a configurable number of scan
+rounds. Hibernating shrinks an idle process' heap to the minimal size needed to
+hold its live data (see `erlang:hibernate/1`); with the `compressed` option the
+heap is additionally stored in compressed form (`erlang:hibernate/2`),
+reclaiming even more memory while the process sleeps.
+
+Idle processes are detected purely from reduction counts, so no per-process
+instrumentation is required: a process whose reduction count is unchanged
+between two consecutive scans did no work in that interval and is considered
+idle. A process is hibernated once it has been idle for `idle_rounds`
+consecutive scans, and is not hibernated again until it does some work (its
+reduction count changes).
+
+The service is not started automatically; start it with `start_link/0,1`.
+""".
+
+-behaviour(gen_server).
+
+-export([start_link/0, start_link/1, stop/0, scan_now/0, info/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+
+-type option() ::
+        {interval, pos_integer()}        %% milliseconds between scans
+      | {idle_rounds, pos_integer()}     %% idle scans before hibernating
+      | {compressed, boolean()}          %% also compress the heap
+      | {exclude, [pid()] | fun((pid()) -> boolean())}.
+-export_type([option/0]).
+
+-define(DEFAULT_INTERVAL, 5000).
+-define(DEFAULT_IDLE_ROUNDS, 2).
+
+-record(state, {interval     :: pos_integer(),
+                idle_rounds  :: pos_integer(),
+                compressed   :: boolean(),
+                exclude      :: fun((pid()) -> boolean()),
+                %% pid => last observed reduction count
+                reds = #{}   :: #{pid() => non_neg_integer()},
+                %% pid => number of consecutive idle rounds, or the atom
+                %% 'hibernated' once the process has been hibernated
+                idle = #{}   :: #{pid() => non_neg_integer() | hibernated},
+                hibernated_total = 0 :: non_neg_integer(),
+                timer :: reference() | undefined}).
+
+%%% ----------------------------------------------------------------------------
+%%% API
+%%% ----------------------------------------------------------------------------
+
+-doc(#{ equiv => start_link([]) }).
+-spec start_link() -> {ok, pid()} | {error, term()}.
+start_link() ->
+    start_link([]).
+
+-doc """
+Starts the automatic hibernation service and links it to the caller.
+
+Options:
+
+- `{interval, Milliseconds}` - time between scans (default 5000).
+- `{idle_rounds, N}` - number of consecutive idle scans before a process is
+  hibernated (default 2).
+- `{compressed, Boolean}` - if `true`, also compress the heap of hibernated
+  processes (default `false`).
+- `{exclude, PidsOrPred}` - processes to never hibernate, either a list of pids
+  or a predicate `fun((pid()) -> boolean())` returning `true` to exclude.
+""".
+-spec start_link([option()]) -> {ok, pid()} | {error, term()}.
+start_link(Options) when is_list(Options) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Options, []).
+
+-doc "Stops the service.".
+-spec stop() -> ok.
+stop() ->
+    gen_server:stop(?MODULE).
+
+-doc "Forces an immediate scan and returns the number of processes hibernated.".
+-spec scan_now() -> {ok, non_neg_integer()}.
+scan_now() ->
+    gen_server:call(?MODULE, scan_now, infinity).
+
+-doc "Returns statistics about the service.".
+-spec info() -> map().
+info() ->
+    gen_server:call(?MODULE, info, infinity).
+
+%%% ----------------------------------------------------------------------------
+%%% gen_server callbacks
+%%% ----------------------------------------------------------------------------
+
+-doc false.
+init(Options) ->
+    Interval = proplists:get_value(interval, Options, ?DEFAULT_INTERVAL),
+    IdleRounds = proplists:get_value(idle_rounds, Options, ?DEFAULT_IDLE_ROUNDS),
+    Compressed = proplists:get_value(compressed, Options, false),
+    Exclude = make_exclude(proplists:get_value(exclude, Options, [])),
+    State = #state{interval = Interval,
+                   idle_rounds = IdleRounds,
+                   compressed = Compressed,
+                   exclude = Exclude},
+    {ok, arm_timer(State)}.
+
+-doc false.
+handle_call(scan_now, _From, State0) ->
+    {N, State} = scan(State0),
+    {reply, {ok, N}, arm_timer(State)};
+handle_call(info, _From, State) ->
+    Reply = #{interval => State#state.interval,
+              idle_rounds => State#state.idle_rounds,
+              compressed => State#state.compressed,
+              tracked => map_size(State#state.reds),
+              currently_hibernated =>
+                  length([P || {P, hibernated} <- maps:to_list(State#state.idle)]),
+              hibernated_total => State#state.hibernated_total},
+    {reply, Reply, State};
+handle_call(_Req, _From, State) ->
+    {reply, {error, bad_request}, State}.
+
+-doc false.
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+-doc false.
+handle_info(scan, State0) ->
+    {_N, State} = scan(State0),
+    {noreply, arm_timer(State)};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+-doc false.
+terminate(_Reason, _State) ->
+    ok.
+
+%%% ----------------------------------------------------------------------------
+%%% Internal
+%%% ----------------------------------------------------------------------------
+
+make_exclude(Pred) when is_function(Pred, 1) ->
+    Pred;
+make_exclude(Pids) when is_list(Pids) ->
+    Set = sets:from_list(Pids, [{version, 2}]),
+    fun(P) -> sets:is_element(P, Set) end.
+
+arm_timer(State = #state{interval = Interval}) ->
+    State#state{timer = erlang:send_after(Interval, self(), scan)}.
+
+%% One scan round: update the idle bookkeeping and hibernate processes that
+%% have been idle long enough.
+scan(State = #state{reds = PrevReds, idle = PrevIdle,
+                    idle_rounds = Threshold, exclude = Exclude,
+                    compressed = Compressed}) ->
+    Self = self(),
+    HibOpts = case Compressed of
+                  true -> [compressed];
+                  false -> []
+              end,
+    {Reds, Idle, ToHib} =
+        lists:foldl(
+          fun(P, {RAcc, IAcc, HAcc}) when P =:= Self ->
+                  %% Never hibernate the scanner itself.
+                  {RAcc, IAcc, HAcc};
+             (P, {RAcc, IAcc, HAcc}) ->
+                  case erlang:process_info(P, reductions) of
+                      undefined ->
+                          {RAcc, IAcc, HAcc};   %% died between processes/1 and now
+                      {reductions, R} ->
+                          RAcc1 = RAcc#{P => R},
+                          Prev = maps:get(P, PrevReds, undefined),
+                          case Prev =:= R of
+                              true ->
+                                  %% No work since last scan -> idle this round.
+                                  case maps:get(P, PrevIdle, 0) of
+                                      hibernated ->
+                                          {RAcc1, IAcc#{P => hibernated}, HAcc};
+                                      N when N + 1 >= Threshold ->
+                                          case Exclude(P) of
+                                              true ->
+                                                  {RAcc1, IAcc#{P => N + 1}, HAcc};
+                                              false ->
+                                                  {RAcc1, IAcc#{P => hibernated},
+                                                   [P | HAcc]}
+                                          end;
+                                      N ->
+                                          {RAcc1, IAcc#{P => N + 1}, HAcc}
+                                  end;
+                              false ->
+                                  %% Did work (or first time seen) -> active.
+                                  {RAcc1, IAcc#{P => 0}, HAcc}
+                          end
+                  end
+          end,
+          {#{}, #{}, []},
+          erlang:processes()),
+    N = hibernate_all(ToHib, HibOpts, 0),
+    {N, State#state{reds = Reds, idle = Idle,
+                    hibernated_total = State#state.hibernated_total + N}}.
+
+hibernate_all([P | Ps], HibOpts, N) ->
+    Hibernated =
+        try erlang:hibernate(P, HibOpts) of
+            true -> 1;
+            _ -> 0
+        catch
+            _:_ -> 0
+        end,
+    hibernate_all(Ps, HibOpts, N + Hibernated);
+hibernate_all([], _HibOpts, N) ->
+    N.
